9e46b40c4ee50a70f602b2a31007c2839b8454d6
Pietro <54339762+im-pokz@users.noreply.github.com>
2020-12-07 23:56:35 -0300

    Add files via upload

---
+++ pratica/10/TestaAnimal.java
@@ -0,0 +1,30 @@
+package pratica.10;
+
+public class TestaAnimal {
+
+    public static void main(String[] args) {
+        Animal gato = new Animal("gato");
+        Animal vaca = new Animal("vaca");
+        Animal pato = new Animal("pato");
+        Animal sapo = new Animal("sapo");
+        Animal rato = new Animal("rato");
+        Animal galo = new Animal("galo");
+
+        System.out.println(gato);
+
+        gato.adicionaAmigo(vaca);
+        gato.adicionaAmigo(pato);
+        gato.adicionaAmigo(sapo);
+        gato.adicionaAmigo(rato);
+        gato.adicionaAmigo(galo);
+
+        System.out.println("Amigos de gato: " + gato.amigosAnimal());
+
+        System.out.println("Amigos de rato: " + rato.amigosAnimal());
+
+        rato.removeAmigo(gato);
+
+        System.out.println("Amigos de gato: " + gato.amigosAnimal());
+        System.out.println("Amgos de rato: " + rato.amigosAnimal());
+    }
+}
